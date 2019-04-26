02ee7225d77d2c08ddcddd3f98a201be837ab5c9
kallekj <kalle@djervbrant.net>
2019-04-26 14:47:53 +0200

    New version

--- src/timeAnalysis.py
+++ src/timeAnalysis.py
@@ -2,13 +2,14 @@ import pyshark
 from itertools import tee, islice, chain
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.cluster import MeanShift
 from decimal import Decimal
 
+path1Win = r"C:\Users\kalle\Högskolan i Halmstad\Andreas Häggström [andhag16] - Exjobb\Sniffings\SecondSniff\Iphone8Plus_1_Unlocked.pcapng"
+path2Win = r"C:\Users\kalle\Högskolan i Halmstad\Andreas Häggström [andhag16] - Exjobb\Sniffings\SecondSniff\Iphone8Plus_2_Unlocked.pcapng"    
+path3Win = r"C:\Users\kalle\Högskolan i Halmstad\Andreas Häggström [andhag16] - Exjobb\Sniffings\SecondSniff\Iphone7Plus_Unlocked.pcapng"    
 
-
-path = r"C:\Users\kalle\Högskolan i Halmstad\Andreas Häggström [andhag16] - Exjobb\Sniffings\SecondSniff\Iphone7Plus_Unlocked.pcapng"
 path2 = "/home/kalle/Dropbox/KJ/Skola/Exjobb/SecondSniff/Iphone8Plus_2_Unlocked.pcapng"
-
 
 def previous_and_next(some_iterable):
     prevs, items, nexts = tee(some_iterable, 3)
@@ -59,7 +60,6 @@ def getBurstSets(capture):
             burst = []
     return burstSets
 
-
 def getIFATAsBurstSets(capture):
     burstSets = []
     burst = []
@@ -81,7 +81,6 @@ def getIFATAsBurstSets(capture):
             burst = []
     return burstSets
 
-
 def getBurstSetsAsPackets(capture):
     burstSets = []
     burst = []
@@ -101,9 +100,13 @@ def getBurstSetsAsPackets(capture):
             burst = []
     return burstSets
 
+def divide(a,b):
+    try:
+        return a / b
+    except:
+        return 0.0
 
 def calcSignature(IFATArray, binning):
-    signature = []
     binMean1, binMean2, binMean3 = [0, 0, 0]
     numBin1, numBin2, numBin3 = [0, 0, 0]
     for _IFAT in IFATArray:
@@ -116,48 +119,62 @@ def calcSignature(IFATArray, binning):
         else:
             binMean3 = binMean3 + _IFAT
             numBin3 += 1
-    #--- Fixa så den beräknar på burst ---#
-    if numBin1 != 0:
-        signature.append([binning[0], numBin1/len(IFATArray), binMean1/numBin1, numBin1])
-    else:
-        signature.append([binning[0],0,0,0])
-    if numBin2 != 0:
-        signature.append([binning[1], numBin2/len(IFATArray), binMean2/numBin2, numBin2])
-    else:
-        signature.append([binning[1],0,0,0])
-    if numBin3 != 0:
-        signature.append([binning[2], numBin3/len(IFATArray), binMean3/numBin3, numBin3])
-    else:
-        signature.append([binning[2],0,0,0])
-    return signature
+    return [divide(numBin1, len(IFATArray)), divide(binMean1, numBin1), divide(numBin2, len(IFATArray)), divide(binMean2, numBin2), divide(numBin3, len(IFATArray)), divide(binMean3, numBin3)]
 
 def plotData(data):
     data = np.array(data)
     plt.plot(data, marker=".")
     plt.show()
 
-
-
 def main():
 
     try:
         #captureFile = pyshark.FileCapture(input("Enter file path to a .pcapng file: "), display_filter="wlan.sa != 22:22:22:22:22:22 && wlan.da != 22:22:22:22:22:22 && wlan.sa != ac:37:43:3c:d5:53")
         #captureFile = pyshark.FileCapture(input("Enter file path to a .pcapng file: "), display_filter="wlan.sa != 22:22:22:22:22:22 && wlan.da != 22:22:22:22:22:22")
-        captureFile = pyshark.FileCapture(path, display_filter="""wlan.fc.type_subtype eq 4 && 
+        captureFile = pyshark.FileCapture(path1Win, display_filter="""wlan.fc.type_subtype eq 4 && 
                                                                     wlan.sa != d0:16:b4:53:a1:14 && 
                                                                     wlan.sa != 50:01:d9:c6:fe:07 && 
                                                                     wlan.sa != 4a:7a:c2:82:4d:c7 &&
                                                                     wlan.sa != 8c:f5:a3:73:20:56 &&
                                                                     wlan.sa != da:a1:19:9b:5b:9b &&
                                                                     wlan.sa != ac:37:43:3c:d5:53""")
+
+        captureFile2 = pyshark.FileCapture(path2Win, display_filter="""wlan.fc.type_subtype eq 4 && 
+                                                                    wlan.sa != d0:16:b4:53:a1:14 && 
+                                                                    wlan.sa != 50:01:d9:c6:fe:07 && 
+                                                                    wlan.sa != 4a:7a:c2:82:4d:c7 &&
+                                                                    wlan.sa != 8c:f5:a3:73:20:56 &&
+                                                                    wlan.sa != da:a1:19:9b:5b:9b &&
+                                                                    wlan.sa != ac:37:43:3c:d5:53""")
+                                                                    
+        captureFile3 = pyshark.FileCapture(path3Win, display_filter="""wlan.fc.type_subtype eq 4 && 
+                                                                    wlan.sa != d0:16:b4:53:a1:14 && 
+                                                                    wlan.sa != 50:01:d9:c6:fe:07 && 
+                                                                    wlan.sa != 4a:7a:c2:82:4d:c7 &&
+                                                                    wlan.sa != 8c:f5:a3:73:20:56 &&
+                                                                    wlan.sa != da:a1:19:9b:5b:9b &&
+                                                                    wlan.sa != ac:37:43:3c:d5:53""")
+
         #IFATArray = getIFAT(captureFile)
         burstSets = getIFATAsBurstSets(captureFile)
+        burstSets = burstSets + getIFATAsBurstSets(captureFile2)
+        burstSets = burstSets + getIFATAsBurstSets(captureFile3)
         
         #print(burstSets)
+        signatures = []
         for burst in burstSets:
-            print(calcSignature(burst, [0.15, 1.2, 30]))
+            signatures.append(calcSignature(burst, [0.15, 0.7, 2]))
+        
+        ms = MeanShift()
+        ms.fit(signatures)
+        labels = ms.labels_
+        cluster_centers = ms.cluster_centers_
 
-        #IFATArray = np.array(IFATArray)
+        n_clusters_ = len(np.unique(labels))
+
+        print("Num clusters", n_clusters_)
+        print("Cluster centers:\n", cluster_centers)
+        print(labels)
 
     except Exception as e:
         print("Could not find packet file!")
